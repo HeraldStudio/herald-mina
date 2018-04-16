64a044ea9051e3c4ca7e58335aef0aae1a279c54
WolfTungsten <canis.tungsten@foxmail.com>
2018-04-16 19:15:50 +0800

    仪表盘基本完工

--- src/provider.js
+++ src/provider.js
@@ -1,6 +1,10 @@
 import wepy from 'wepy';
 const baseurl = 'https://myseu.cn/ws3';
 
+/**
+ * 将原有wx.request使用promise包装，已达到async/await调用的目的
+ * @param {*} arg 请求参数，和原生的wx.request相同 
+ */
 async function wxRequest(arg) {
     return new Promise((resolve, reject)=>{
         arg.success = (data) => {resolve(data)}
@@ -8,8 +12,18 @@ async function wxRequest(arg) {
         wx.request(arg)
     })
 }
+
+/**
+ * auth
+ * 用于身份认证，本身包含了token写入Storage的过程
+ * @param {string} cardnum  
+ * @param {string} password 
+ * 返回一个对象 {
+ *      success:{bool} 是否成功
+ *      msg:{string} 如果不成功则包含错误信息
+ * }
+ */
 async function auth(cardnum, password) {
-//登录并设置token
     try {
         let authResult = await wxRequest({
             url:baseurl+'/auth',
@@ -40,19 +54,36 @@ function getToken() {
     
 }
 
+/**
+ * 注销
+ * 最好加上delete请求，暂时没有加
+ * 注销的同时会清空所有的用户数据
+ */
 function deAuth() {
     wx.clearStorageSync()
 }
 
+/**
+ * 请求
+ * 对于逻辑相关的请求使用request发起
+ * 包装了wxRequest
+ * 请求header中会自动添加token
+ * 将herald-webservice返回格式拆包
+ *  - 请求成功时返回result
+ *  - 请求失败时返回false
+ */
 async function request(url, method, data) {
     url = baseurl + url
     if (method === 'GET' || method === 'DELETE' && data) {
         url += '?'
         for (let key in data) {
-            url += `${key}=${data[key]}&`
+            if(data[key]){
+                url += `${key}=${data[key]}&`
+            }
         }
+        data = null
     }
-    token = wx.getStorageSync('herald-token')
+    let token = wx.getStorageSync('herald-token')
     try {
     let response = await wxRequest({url, method, data, header:{token}, dataType:'json'})
     response = response.data
@@ -68,6 +99,13 @@ async function request(url, method, data) {
         return false
     }
 }
+
+/**
+ * UI数据请求
+ * 包装了request
+ * 函数本身不返回任何内容
+ * hook作为回调函数传入，接收内容和request返回相同
+ */
 
 function fetchUIdata(url, method, data, hook) {
     //Step1.读取缓存
@@ -89,4 +127,41 @@ function fetchUIdata(url, method, data, hook) {
     })
 }
 
-export {auth, deAuth, fetchUIdata, request}
+/**
+ * ----------------------------------
+ * 下面的内容是fetchUIdata对特定API的封装
+ * ----------------------------------
+ */
+
+function userInfoApi(hook){
+    fetchUIdata('/api/user', 'GET', null, hook);
+}
+
+/**
+ * 一卡通查询API
+ * 充值api直接调用request发起
+ * @param {number} 查询日期yyyy-M-d格式
+ * @param {number} 查询页码
+ */
+ function allinoneCardApi(hook, date, page){
+    fetchUIdata('/api/card', 'GET', {date, page}, hook);
+ }
+
+ function peApi(hook){
+    fetchUIdata('/api/pe', 'GET', null, hook);
+ }
+
+ function gpaApi(hook){
+    fetchUIdata('/api/gpa', 'GET', null, hook);
+ }
+
+ function lectureApi(hook){
+    fetchUIdata('/api/lecture', 'GET', null, hook);
+ }
+
+ function srtpApi(hook){
+    fetchUIdata('/api/srtp', 'GET', null, hook);
+ }
+
+export {auth, deAuth, fetchUIdata, request, 
+    allinoneCardApi, userInfoApi, peApi, gpaApi, lectureApi, srtpApi}
